173e1bb1be66f889a0b5fa282fdfd4b603f867f4
Cinthia-hub <ccamibrm@gmail.com>
2026-04-26 23:05:07 -0600

    [CREATED]: Estructura base frontend lista

---
+++ .next/dev/server/server-reference-manifest.js
@@ -0,0 +1,1 @@
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"l9Qt0W5sFRCjvY+nz9cZDX4iRjQnqHazu/GXpn27Q7E=\"\n}"
---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,3 @@
+export default function Page() {
+  return <div>Dashboard de Prueba</div>;
+}
